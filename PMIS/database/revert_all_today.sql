d8373794d244330634d65ae41a59d99d6271dd58
OyebamijiEmmanuel98 <justemmy005@gmail.com>
2026-07-06 17:03:06 +0100

    Delete PMIS directory

--- PMIS/database/revert_all_today.sql
+++
@@ -1,6 +0,0 @@
--- Revert script for changes made today
-USE pmis_dominion;
-
--- 5. Delete test leave applications using Short Leave, then remove Short Leave type
-DELETE FROM `leave_applications` WHERE `leave_type_id` = (SELECT `id` FROM `leave_types` WHERE `leave_name` = 'Short Leave of Absence');
-DELETE FROM `leave_types` WHERE `leave_name` = 'Short Leave of Absence';
